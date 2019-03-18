2f40787e04505789751bf1426200587da30cc2b6
tuanbi97 <tuanbi97@gmail.com>
2019-03-18 15:59:23 +0700

    misc

--- mask_rcnn_pytorch/loader.cpp
+++ mask_rcnn_pytorch/loader.cpp
@@ -19,6 +19,7 @@ void Loader::Prepare()
     this->num_classes_ = this->class_infos_.size();
     for (std::uint16_t i; i < this->num_classes_; ++i)
     {
+        std::cout<<"class_name\n";
         this->class_id_from_class_name_map_[this->class_infos_[i].class_name] = i;
         this->class_ids_.push_back(i);
     }
