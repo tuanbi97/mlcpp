0df3b29c2b6f439e21ceba53c435728ad9af843a
tuanbi97 <tuanbi97@gmail.com>
2019-03-19 00:21:01 +0700

    misc

--- mask_rcnn_pytorch/vehicleloader.cpp
+++ mask_rcnn_pytorch/vehicleloader.cpp
@@ -179,7 +179,7 @@ void VehicleLoader::LoadData()
     int index = 0;
     for (auto &row : parser)
     {
-        if (index > 1) break;
+        if (index > 0) break;
         if (!header_parsed)
         {
             for (auto &field : row)
