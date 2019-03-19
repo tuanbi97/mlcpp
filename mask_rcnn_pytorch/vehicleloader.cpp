3d3a03da1d7dfb1efe3690d844a605551edbbd52
tuanbi97 <tuanbi97@gmail.com>
2019-03-19 20:14:42 +0700

    misc

--- mask_rcnn_pytorch/vehicleloader.cpp
+++ mask_rcnn_pytorch/vehicleloader.cpp
@@ -31,7 +31,7 @@ void VehicleLoader::_ParseContours(const std::string &string_points, ImageInfo &
             if (level == 1){
                 std::string ss = string_points.substr(previous_pos, i - previous_pos);
                 std::vector<std::int32_t> contour(0);
-                image_info.contours.push_back(contour)
+                image_info.contours.push_back(contour);
                 std::vector<std::string> v_points_string = SplitString(ss, ',');
                 for (auto &s : v_points_string)
                 {
